fad7275a41392088edf0814755c0e0df8da97ebd
Victor K. Maina <vk13runic@gmail.com>
2022-04-13 04:19:52 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<RouterView></RouterView>
+</template>
+
+<script>
+import { RouterView } from "vue-router";
+
+export default {
+    components: {
+        RouterView
+    }
+}
+</script>
+
+
+<style lang="scss"></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+<h1>FEM Family Church Mombasa</h1>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style lang="scss"></style>
