fd1cb4aae14e34bf91ade8946325397b73754708
Victor K. Maina <vk13runic@gmail.com>
2022-04-14 11:59:57 +0300

    create heading component for homepage hero section

--- src/views/Home/HeroSection/Heading.vue
+++ src/views/Home/HeroSection/Heading.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="heading">
-        <h1 class="display-1 margin-bottom-1">A place for you to belong.</h1>
-        <p class="display-3">
+        <h1 class="display-1 margin-bottom-2">A place for you to belong.</h1>
+        <p class="display-3 margin-bottom-3">
             Church is more than a building. It’s a family. Welcome to FEM Family
             Church Mombasa.
         </p>
@@ -19,5 +19,12 @@ export default {};
 div#heading {
     width: 100%;
     max-width: 500px;
+    padding: 3rem;
+    border-radius: 1rem;
+    background-color: config.color('light-2');
+
+    button {
+        width: 100%;
+    }
 }
 </style>
